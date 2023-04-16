484265fa29c4a270f033e964ef15300898c104d1
Rakhavendra S M <rakhavendra631999@gmail.com>
2023-04-16 20:14:05 +0530

    Loader changed.

--- src/util/Loader/Loader.jsx
+++ src/util/Loader/Loader.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import loader from "../../asset/image/loader.gif"
 
-export default () => {
-    return (
-        <div className="">
-             <div className="loaderAlign">
-                <img width="50px" src={loader} alt="" /><br />
+const Loader = ({ size = '80px' }) => {
+    return (<div><div className="modal d-block" id="loader" tabIndex="-1" aria-labelledby="loader" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content p-4">
+
+                <div className="modal-body text-center">
+
+                    <h2 className="font26 fw-normal breathe">Loading</h2>
+
+                    <img src={loader} alt="loading..." />
+                </div>
+
             </div>
         </div>
-    )
+    </div>
+        <div className="modal-backdrop show"></div></div>)
 }
+
+export default Loader;
